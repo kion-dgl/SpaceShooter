65ef4285d5802e3089ff665a161f345f518fa3e2
Benjamin Collins <kion@dashgl.com>
2017-02-16 21:06:21 +0900

    gameover

---
+++ 11_player_hit/shader/display.glsl
@@ -0,0 +1,9 @@
+attribute vec2 coord2d;
+attribute vec2 texcoord;
+varying vec2 f_texcoord;
+uniform mat4 ortho;
+
+void main(void) {
+  gl_Position = ortho * vec4(coord2d, 0.0, 1.0);
+  f_texcoord = texcoord;
+}
